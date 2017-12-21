e60101955da8de1a115224889f896fb40fff8a98
ruanzetao <ruanzetao@gmail.com>
2017-12-21 20:37:19 +0700

    done

--- crawler.php
+++ crawler.php
@@ -1,23 +1,25 @@
 <?php
 require "simple_html_dom.php";
+//$count=0;
 
 	function upViewZing(){
-		$html=file_get_html("https://news.zing.vn/hanh-trinh-hoi-sinh-xe-porsche-901-1964-huyen-thoai-da-cu-nat-post805713.html");
+		$html=file_get_html("https://news.zing.vn/morgan-ev3-bat-dau-duoc-san-xuat-tu-2018-post805717.html");
 		//echo $html;
 		
 
 	}
 
 	function thisLoop(){
-		for($i=0;$i<5;$i++){
+		for($i=0;$i<1000;$i++){
 			upViewZing();
-			sleep(2);
-			echo "This page will refresh!<br>";
-			header("Refresh: 5; url=index.html");
-			echo "done!";
+			//sleep(2);
+			//echo "This page will refresh!<br>";
+			//header("Refresh: 0; url=https://news.zing.vn/morgan-ev3-bat-dau-duoc-san-xuat-tu-2018-post805717.html");
+			//$count=$count+1;
 		}
 	}
 
 	thisLoop();
+	//echo $count;
 
 ?>
