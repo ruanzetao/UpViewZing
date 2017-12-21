a24b77888d52dbf45d0d92d9a6d92946ffabd8e1
ruanzetao <ruanzetao@gmail.com>
2017-12-21 20:10:49 +0700

    done

---
+++ crawler.php
@@ -0,0 +1,23 @@
+<?php
+require "simple_html_dom.php";
+
+	function upViewZing(){
+		$html=file_get_html("https://news.zing.vn/hanh-trinh-hoi-sinh-xe-porsche-901-1964-huyen-thoai-da-cu-nat-post805713.html");
+		//echo $html;
+		
+
+	}
+
+	function thisLoop(){
+		for($i=0;$i<5;$i++){
+			upViewZing();
+			sleep(2);
+			echo "This page will refresh!";
+			header("Refresh: 5; url=index.html");
+			// echo "done!";
+		}
+	}
+
+	thisLoop();
+
+?>
